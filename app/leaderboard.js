3cfae72f2762f66a7acd857bcf3f2a7957ceb507
jleong8 <jleong8@hawaii.edu>
2017-02-28 11:47:15 -1000

    Task 4

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,3 +1,18 @@
-/**
- * Created by Jason on 2/28/2017.
- */
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function(){
+      console.log("You clicked a .player element");
+    }
+  });
+}
+
+if(Meteor.isServer){
+  // this code only runs on the server
+}
